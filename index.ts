bfd7162a8529b54ea6d33aa4bf04e3aefd62f336
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-13 00:18:44 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import express, { Express, Request, Response } from "express";  
 
+import dotenv from "dotenv";
+
+import * as database from "./config/database";
+
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+dotenv.config();
+
+database.connect();
 
 app.set("views", `./views`);
 app.set("view engine", "pug");
