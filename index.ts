eb6d7954d14d953ec56f0ea6a64713ffee407155
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-12 23:43:19 +0700

    Tạo các routes cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("OK")
+import express, { Express, Request, Response } from "express";  
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Trang chủ");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
