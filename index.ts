39cf83b8b7816c66780c377ef037e928e167991a
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-13 00:12:38 +0700

    Cài đặt PUG

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+app.set("views", `./views`);
+app.set("view engine", "pug");
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Trang chủ");
+  res.render("client/pages/home/index");
 });
 
 app.listen(port, () => {
